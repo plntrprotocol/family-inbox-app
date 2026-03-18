ef518d6af100560e66db39cc4b1d26cee1cf9ac7
Johann <johann@Mac.lan>
2026-03-18 16:43:40 -0400

    Fix functions for Pages

--- functions/api/messages.js
+++ functions/api/messages.js
@@ -1,4 +1,4 @@
-export async function onRequestGet(): Promise<Response> {
+export function onRequestGet() {
   const messages = [
     { sender: "Palantir", recipient: "Isildur", body: "Welcome to The Roost, sibling!", created_at: "2026-03-18" },
     { sender: "Museah", recipient: "Isildur", body: "So glad you're here.", created_at: "2026-03-18" }
